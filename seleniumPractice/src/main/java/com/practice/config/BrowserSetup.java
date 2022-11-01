d21819e825e3c1c5481f6598b7d44744164e8526
thipparaj <thipparaj@gmail.com>
2022-11-01 19:29:14 +0530

    test browser launch configuration

--- seleniumPractice/src/main/java/com/practice/config/BrowserSetup.java
+++ seleniumPractice/src/main/java/com/practice/config/BrowserSetup.java
@@ -1,9 +1,24 @@
 package com.practice.config;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BrowserSetup {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeOptions chrome = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(chrome);
+		driver.get("https://www.youtube.com/watch?v=LPT7v69guVY&ab_channel=AutomationStepbyStep");
+		Thread.sleep(3000);
+		String title = driver.getTitle();
+		System.out.println(title);
+		Thread.sleep(3000);
+		driver.quit();
 	}
 
 }
